4721899e63ecefc0f35542e54187aa86167a9be0
Arthur Lage <arthurlage2006@gmail.com>
2025-07-29 16:30:23 -0300

    create dialog menu

--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,7 @@
+/// SOUND
+let playingSoundsController = {
+    hover: false
+}
 
 const hoverSound = new Howl({
     src: ['../assets/audio/hover.wav'],
@@ -10,6 +14,55 @@ const options = document.querySelectorAll(".option")
 
 options.forEach(opt => {
     opt.addEventListener("mouseover", () => {
-        hoverSound.play();
+        if (playingSoundsController.hover == false) {
+            hoverSound.play();
+            playingSoundsController.hover = true
+        }
     })
 })
+
+hoverSound.on('end', () => {
+    playingSoundsController.hover = false
+})
+
+// DIALOG
+
+const dialogOverlay = document.querySelector(".dialog-overlay")
+const dialogClose = document.querySelector(".dialog-close")
+
+const menuRulesDialog = document.querySelector(".rules")
+const menuOptionsDialog = document.querySelector(".options")
+const menuCreditsDialog = document.querySelector(".credits")
+
+const menuRulesButton = document.querySelector("#menu-rules")
+const menuOptionsButton = document.querySelector("#menu-options")
+const menuCreditsButton = document.querySelector("#menu-credits")
+
+menuRulesButton.addEventListener("click", () => {
+    resetMenus()
+    menuRulesDialog.classList.add("active")
+    dialogOverlay.classList.add("active")
+})
+
+menuOptionsButton.addEventListener("click", () => {
+    resetMenus()
+    menuOptionsDialog.classList.add("active")
+    dialogOverlay.classList.add("active")
+})
+
+menuCreditsButton.addEventListener("click", () => {
+    resetMenus()
+    menuCreditsDialog.classList.add("active")
+    dialogOverlay.classList.add("active")
+})
+
+dialogClose.addEventListener("click", () => {
+    resetMenus()
+    dialogOverlay.classList.remove('active')
+})
+
+const resetMenus = () => {
+    menuRulesDialog.classList.remove("active")
+    menuCreditsDialog.classList.remove("active")
+    menuOptionsDialog.classList.remove("active")
+}
